ec0e27a0afbb5105136ee1e33f74d456fbf0023e
Aleksander Adamkowski <aleks.adamkowski@gmail.com>
2025-02-14 18:37:07 +0100

    WIP: Incomplete fix for Task A3_2 refactoring from A3_1

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -18,13 +18,47 @@ from sklearn.preprocessing import StandardScaler
 class LinearRegression(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
-        self.input_size = input_size
         self.linear = nn.Linear(input_size, output_size)
 
     def forward(self, x):
-        x = x.view(-1, self.input_size)
         x = torch.sigmoid(self.linear(x))
         return x
+
+
+class ModelTrainer:
+    @staticmethod
+    def support_vector_machine(load_data_func):
+        train_data, train_labels, test_data, test_labels = load_data_func()
+        model = svm.SVC(kernel="linear")
+        model.fit(train_data, train_labels)
+        predictions = model.predict(test_data)
+        accuracy = accuracy_score(test_labels, predictions)
+        mse = mean_squared_error(test_labels, predictions)
+        print(f"SVM Accuracy: {accuracy*100:.2f}%")
+        return [mse] * 10
+
+    @staticmethod
+    def random_forest(load_data_func):
+        train_data, train_labels, test_data, test_labels = load_data_func()
+        model = RandomForestClassifier(max_depth=10)
+        model.fit(train_data, train_labels)
+        predictions = model.predict(test_data)
+        accuracy = accuracy_score(test_labels, predictions)
+        mse = mean_squared_error(test_labels, predictions)
+        print(f"Random Forest Accuracy: {accuracy*100:.2f}%")
+        return [mse] * 10
+
+    @staticmethod
+    def visualize_errors(lr_mse, svm_mse, rf_mse):
+        epochs = range(1, len(lr_mse) + 1)
+        plt.plot(epochs, lr_mse, "r", label="LR")
+        plt.plot(epochs, svm_mse, "g", label="SVM")
+        plt.plot(epochs, rf_mse, "b", label="RF")
+        plt.xlabel("Epoch")
+        plt.ylabel("MSE")
+        plt.legend(loc="upper right")
+        plt.title("MSE vs Epoch")
+        plt.show()
 
 
 class TaskA3_I:
@@ -70,14 +104,14 @@ class TaskA3_I:
             linearRegression.parameters(), LEARNING_RATE
         )  # defining the optimizer
 
-        lr_mse = []
+        mse_list = []
         for epoch in range(EPOCHS):
             for images, labels in train_loader:
                 images, labels = images.to(device), labels.to(device)
                 # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
                 optimizer.zero_grad()
                 # get output from the model, given the inputs
-                outputs = linearRegression(images)
+                outputs = linearRegression(images.view(-1, 28 * 28))
 
                 labels_one_hot = nn.functional.one_hot(labels, num_classes=10).float()
                 # get loss for the predicted output
@@ -86,52 +120,84 @@ class TaskA3_I:
                 loss.backward()
                 # update parameters
                 optimizer.step()
-            lr_mse.append(loss.item())
+            mse_list.append(loss.item())
             correct = 0
             for images, labels in test_loader:
                 images, labels = images.to(device), labels.to(device)
-                outputs = linearRegression(images)
+                outputs = linearRegression(images.view(-1, 28 * 28))
                 _, predicted = torch.max(outputs.data, 1)
                 correct += (predicted == labels).sum()
             accuracy = 100 * (correct.item()) / len(test_dataset)
             print("Epoch: {}. Loss: {}. Accuracy: {}".format(epoch, loss.item(), accuracy))
-        return lr_mse
+        return mse_list
 
     def support_vector_machine():
-        train_data, train_labels, test_data, test_labels = TaskA3_I.load_data()
-
-        svm_classifier = svm.SVC(kernel="linear")
-        svm_classifier.fit(train_data, train_labels)
-        svm_predictions = svm_classifier.predict(test_data)
-        svm_accuracy = accuracy_score(test_labels, svm_predictions)
-        mse = mean_squared_error(test_labels, svm_predictions)
-        print("SVM Accuracy: {}".format(svm_accuracy))
-        return [mse] * 10
+        return ModelTrainer.support_vector_machine(TaskA3_I.load_data)
 
     def random_forest():
-        train_data, train_labels, test_data, test_labels = TaskA3_I.load_data()
-        rf_classifier = RandomForestClassifier(max_depth=10)
-        rf_classifier.fit(train_data, train_labels)
-        rf_predictions = rf_classifier.predict(test_data)
-        rf_accuracy = accuracy_score(test_labels, rf_predictions)
-        mse = mean_squared_error(test_labels, rf_predictions)
-        print("Random Forest Accuracy: {}".format(rf_accuracy))
-        return [mse] * 10
+        return ModelTrainer.random_forest(TaskA3_I.load_data)
 
     def visualize_errors(lr_mse, svm_mse, rf_mse):
-        epochs = range(1, len(lr_mse) + 1)
-        plt.plot(epochs, lr_mse, "r", label="LR")
-        plt.plot(epochs, svm_mse, "g", label="SVM")
-        plt.plot(epochs, rf_mse, "b", label="RF")
-        plt.xlabel("Epoch")
-        plt.ylabel("MSE")
-        plt.legend(loc="upper right")
-        plt.title("MSE vs Epoch")
-        plt.show()
+        ModelTrainer.visualize_errors(lr_mse, svm_mse, rf_mse)
 
 
 class TaskA3_II:
-    pass
+    def load_data():
+        df = pd.read_csv("./Lab3/Data/seattle-weather.csv")
+        df = df.dropna()
+
+        df["weather"] = df["weather"].map({"drizzle": 0, "rain": 1, "sun": 2, "snow": 3, "fog": 4})
+
+        X = df[["precipitation", "temp_max", "temp_min", "wind"]]
+        y = df["weather"]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        return X_train, X_test, y_train, y_test
+
+    def linear_regression():
+        X_train, X_test, y_train, y_test = TaskA3_II.load_data()
+        INPUT_SIZE = X_train.shape[1]
+        OUTPUT_SIZE = len(
+            pd.get_dummies(y_train).columns
+        )  # Ensure output size matches number of classes
+        LEARNING_RATE = 0.01
+        EPOCHS = 10
+        model = LinearRegression(INPUT_SIZE, OUTPUT_SIZE)
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), LEARNING_RATE)
+
+        X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+        y_train_tensor = torch.tensor(pd.get_dummies(y_train).values, dtype=torch.float32)
+        X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+        y_test_tensor = torch.tensor(pd.get_dummies(y_test).values, dtype=torch.float32)
+        mse_list = []
+
+        for epoch in range(EPOCHS):
+            optimizer.zero_grad()
+            outputs = model(X_train_tensor)
+            loss = criterion(outputs, y_train_tensor)
+            loss.backward()
+            optimizer.step()
+            mse_list.append(loss.item())
+            print(f"Epoch: {epoch}")
+
+        model.eval()
+        with torch.no_grad():
+            outputs = model(X_test_tensor)
+            mse = criterion(outputs, y_test_tensor).item()
+            mse_list.append(mse)
+
+        return mse_list
+
+    def support_vector_machine():
+        X_train, X_test, y_train, y_test = TaskA3_II.load_data()
+        return ModelTrainer.support_vector_machine(lambda: (X_train, y_train, X_test, y_test))
+
+    def random_forest():
+        X_train, X_test, y_train, y_test = TaskA3_II.load_data()
+        return ModelTrainer.random_forest(lambda: (X_train, y_train, X_test, y_test))
+
+    def visualize_errors(lr_mse, svm_mse, rf_mse):
+        ModelTrainer.visualize_errors(lr_mse, svm_mse, rf_mse)
 
 
 class TaskA3_IV:
@@ -210,9 +276,14 @@ class TaskA3_IV:
 
 
 if __name__ == "__main__":
-    # lr_mse = TaskA3_I.linear_regression()
-    # svm_mse = TaskA3_I.support_vector_machine()
-    # rf_mse = TaskA3_I.random_forest()
-    # TaskA3_I.visualize_errors(lr_mse, svm_mse, rf_mse)
+    # mnist_lr_mse = TaskA3_I.linear_regression()
+    # mnist_svm_mse = TaskA3_I.support_vector_machine()
+    # mnist_rf_mse = TaskA3_I.random_forest()
+    # TaskA3_I.visualize_errors(mnist_lr_mse, mnist_svm_mse, mnist_rf_mse)
 
-    TaskA3_IV.k_means()
+    weather_lr_mse = TaskA3_II.linear_regression()
+    weather_svm_mse = TaskA3_II.support_vector_machine()
+    weather_rf_mse = TaskA3_II.random_forest()
+    TaskA3_II.visualize_errors(weather_lr_mse, weather_svm_mse, weather_rf_mse)
+
+    # TaskA3_IV.k_means()
