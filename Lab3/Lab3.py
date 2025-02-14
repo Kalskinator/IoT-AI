c10ab7e5c8efa7d7e72c9eebfd9ea0c454150ad3
Aleksander Adamkowski <aleks.adamkowski@gmail.com>
2025-02-14 14:37:30 +0100

    Completed Task A3_4

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -5,9 +5,14 @@ import torchvision.transforms as transforms
 from torchvision import datasets
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 class LinearRegression(nn.Module):
@@ -21,11 +26,16 @@ class LinearRegression(nn.Module):
         x = torch.sigmoid(self.linear(x))
         return x
 
-class TaskA3:
+
+class TaskA3_I:
 
     def load_data():
-        train_dataset = datasets.MNIST(root="./Lab3/Data", train=True, transform=transforms.ToTensor(), download=True)
-        test_dataset = datasets.MNIST(root="./Lab3/Data", train=False, transform=transforms.ToTensor())
+        train_dataset = datasets.MNIST(
+            root="./Lab3/Data", train=True, transform=transforms.ToTensor(), download=True
+        )
+        test_dataset = datasets.MNIST(
+            root="./Lab3/Data", train=False, transform=transforms.ToTensor()
+        )
         train_data = train_dataset.data[:10000].view(-1, 28 * 28).numpy()
         train_labels = train_dataset.targets[:10000].numpy()
         test_data = test_dataset.data.view(-1, 28 * 28).numpy()
@@ -33,8 +43,12 @@ class TaskA3:
         return train_data, train_labels, test_data, test_labels
 
     def linear_regression():
-        train_dataset = datasets.MNIST(root="./Lab3/Data", train=True, transform=transforms.ToTensor(), download=True)
-        test_dataset = datasets.MNIST(root="./Lab3/Data", train=False, transform=transforms.ToTensor())
+        train_dataset = datasets.MNIST(
+            root="./Lab3/Data", train=True, transform=transforms.ToTensor(), download=True
+        )
+        test_dataset = datasets.MNIST(
+            root="./Lab3/Data", train=False, transform=transforms.ToTensor()
+        )
 
         BATCH_SIZE = 32
         N_INPUTS = 28 * 28
@@ -52,7 +66,9 @@ class TaskA3:
         linearRegression = LinearRegression(N_INPUTS, N_OUTPUTS)
 
         loss_function = nn.MSELoss()
-        optimizer = optim.Adam(linearRegression.parameters(), LEARNING_RATE)  # defining the optimizer
+        optimizer = optim.Adam(
+            linearRegression.parameters(), LEARNING_RATE
+        )  # defining the optimizer
 
         lr_mse = []
         for epoch in range(EPOCHS):
@@ -82,7 +98,7 @@ class TaskA3:
         return lr_mse
 
     def support_vector_machine():
-        train_data, train_labels, test_data, test_labels = TaskA3.load_data()
+        train_data, train_labels, test_data, test_labels = TaskA3_I.load_data()
 
         svm_classifier = svm.SVC(kernel="linear")
         svm_classifier.fit(train_data, train_labels)
@@ -93,28 +109,110 @@ class TaskA3:
         return [mse] * 10
 
     def random_forest():
-        train_data, train_labels, test_data, test_labels = TaskA3.load_data()
+        train_data, train_labels, test_data, test_labels = TaskA3_I.load_data()
         rf_classifier = RandomForestClassifier(max_depth=10)
         rf_classifier.fit(train_data, train_labels)
         rf_predictions = rf_classifier.predict(test_data)
         rf_accuracy = accuracy_score(test_labels, rf_predictions)
-        mse = mean_squared_error(test_labels,rf_predictions)
+        mse = mean_squared_error(test_labels, rf_predictions)
         print("Random Forest Accuracy: {}".format(rf_accuracy))
         return [mse] * 10
 
     def visualize_errors(lr_mse, svm_mse, rf_mse):
         epochs = range(1, len(lr_mse) + 1)
-        plt.plot(epochs, lr_mse, 'r', label='LR')
-        plt.plot(epochs, svm_mse, 'g', label='SVM')
-        plt.plot(epochs, rf_mse, 'b', label='RF')
-        plt.xlabel('Epoch')
-        plt.ylabel('MSE')
-        plt.legend(loc='upper right')
-        plt.title('MSE vs Epoch')
+        plt.plot(epochs, lr_mse, "r", label="LR")
+        plt.plot(epochs, svm_mse, "g", label="SVM")
+        plt.plot(epochs, rf_mse, "b", label="RF")
+        plt.xlabel("Epoch")
+        plt.ylabel("MSE")
+        plt.legend(loc="upper right")
+        plt.title("MSE vs Epoch")
         plt.show()
 
+
+class TaskA3_II:
+    pass
+
+
+class TaskA3_IV:
+
+    def k_means():
+
+        df = pd.read_csv(
+            "./Lab3/Data/penguins.csv", usecols=["species", "bill_length_mm", "bill_depth_mm"]
+        )
+
+        df = df.dropna()
+
+        X = df[["bill_length_mm", "bill_depth_mm"]]
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+
+        inertias = []
+        # testing the data on 1 to 11 clusters and calculating the inertia
+        for i in range(1, 11):
+            kmeans = KMeans(n_clusters=i)
+            kmeans.fit(X)
+            inertias.append(kmeans.inertia_)
+
+        plt.plot(range(1, 11), inertias, marker="o")
+        plt.title("Elbow method")
+        plt.xlabel("Number of clusters")
+        plt.ylabel("Inertia")
+        plt.show()
+
+        kmeans = KMeans(n_clusters=3, init="k-means++", n_init=20, random_state=42)
+        # kmeans = KMeans(n_clusters=3)
+        df["cluster"] = kmeans.fit_predict(X)
+
+        # Map cluster labels to species names
+        cluster_to_species = {0: "Adelie", 2: "Chinstrap", 1: "Gentoo"}
+        df["species_cluster"] = df["cluster"].map(lambda x: cluster_to_species[x])
+
+        # Visualizing the clusters
+        for cluster, color in zip(cluster_to_species.values(), ["royalblue", "orange", "green"]):
+            cluster_data = df[df["species_cluster"] == cluster]
+            plt.scatter(
+                cluster_data["bill_length_mm"],
+                cluster_data["bill_depth_mm"],
+                label=cluster,
+                color=color,
+            )
+
+        plt.xlabel("Bill Length (mm)")
+        plt.ylabel("Bill Depth (mm)")
+        plt.title("K-Means Clustering of Penguins")
+        plt.legend(loc="upper right")
+        plt.show()
+
+        # Inverse transform the centroids back to the original scale
+        centroids = scaler.inverse_transform(kmeans.cluster_centers_)
+        for cluster, color in zip(cluster_to_species.values(), ["royalblue", "orange", "green"]):
+            cluster_data = df[df["species_cluster"] == cluster]
+            plt.scatter(
+                cluster_data["bill_length_mm"],
+                cluster_data["bill_depth_mm"],
+                label=cluster,
+                color=color,
+            )
+        plt.scatter(centroids[:, 0], centroids[:, 1], c="black", marker="X", label="Centroids")
+        plt.xlabel("Bill Length (mm)")
+        plt.ylabel("Bill Depth (mm)")
+        plt.title("K-Means Clustering of Penguins with Centroids")
+        plt.legend(loc="upper right")
+        plt.show()
+
+        # Evaluate the model
+        correct_labels = sum(df["species"] == df["species_cluster"])
+        accuracy = correct_labels / len(df)
+        print(f"K-Means Clustering Accuracy: {accuracy*100:.2f}%")
+
+
 if __name__ == "__main__":
-    lr_mse = TaskA3.linear_regression()
-    svm_mse = TaskA3.support_vector_machine()
-    rf_mse = TaskA3.random_forest()
-    TaskA3.visualize_errors(lr_mse, svm_mse, rf_mse)
+    # lr_mse = TaskA3_I.linear_regression()
+    # svm_mse = TaskA3_I.support_vector_machine()
+    # rf_mse = TaskA3_I.random_forest()
+    # TaskA3_I.visualize_errors(lr_mse, svm_mse, rf_mse)
+
+    TaskA3_IV.k_means()
